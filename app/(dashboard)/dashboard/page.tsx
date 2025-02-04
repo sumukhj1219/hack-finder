f35e505b7830dce7ae2d2d655a5fb0d8a2c63466
sumukhj1219 <sumukhjoshi75@gmail.com>
2025-02-05 00:10:59 +0530

    reinit

---
+++ app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import IdeaGenerator from '@/components/dashboardComponent/ideaComponent'
+import React from 'react'
+
+const DashboardPage = () => {
+  return (
+    <div>
+        <IdeaGenerator />
+    </div>
+  )
+}
+
+export default DashboardPage
---
+++ lib/auth.ts
@@ -0,0 +1,15 @@
+import { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt", 
+  },
+};
